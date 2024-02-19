1fc553f571da14b6dfed35aca33eb3718d23ea2c
Mocsid <mocsid@outlook.com>
2024-02-19 21:15:21 +0800

    Change parameter order

--- helper-functions.php
+++ helper-functions.php
@@ -6,9 +6,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 if ( ! function_exists('moc_log') ) {
 
-    function moc_log( $message, $level = 'INFO' ) {
+    function moc_log( $level = 'INFO', $message ) {
         if ( class_exists( 'MOC_WP_Logger' ) && method_exists( 'MOC_WP_Logger', 'log_message' ) ) {
-            MOC_WP_Logger::log_message( $message, $level );
+            MOC_WP_Logger::log_message( $level, $message );
         }
     }
 
