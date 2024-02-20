78c552301ae2be75ca74577ac34bd53a7555e729
Mocsid <mocsid@outlook.com>
2024-02-20 17:41:20 +0800

    Fixed parameter error in the function moc_log

--- helper-functions.php
+++ helper-functions.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 if ( ! function_exists('moc_log') ) {
 
-    function moc_log( $level = 'INFO', $message ) {
+    function moc_log( $message, $level = 'INFO' ) {
         if ( class_exists( 'MOC_WP_Logger' ) && method_exists( 'MOC_WP_Logger', 'log_message' ) ) {
             MOC_WP_Logger::log_message( $level, $message );
         }
